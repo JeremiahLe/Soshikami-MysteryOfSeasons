c3d7b70960ae184d3c0d9853cf547fb9556f6b27
Jeremiah Le <jeremiahle789@gmail.com>
2020-02-21 13:37:15 -0500

    misc commit 0.01 - better documentation in CreatureScript

--- Soshikami - Mystery of Seasons/Assets/Scripts/CreatureScript.cs
+++ Soshikami - Mystery of Seasons/Assets/Scripts/CreatureScript.cs
@@ -4,39 +4,49 @@ using UnityEngine;
 
 public class CreatureScript : MonoBehaviour
 {
-    // general stats
-    [SerializeField]
-    private enum NPCType {docileNPC, neutralNPC, enemyNPC }
-    private enum Element { Fire, Water, Wind, Earth, Lightning, Dark, Light}
-    private enum Status { Poisoned, Burning, Slowed, Stunned, Cursed, Immune }
-    [SerializeField]
-    public float health;
+    // Enumerators for statuses - Type of NPC, Elemental Affinity, Status, States
+    enum NPCType {docileNPC, neutralNPC, enemyNPC }
+    enum Element { Fire, Water, Wind, Earth, Lightning, Dark, Light }
+    enum Status { Poisoned, Burning, Slowed, Stunned, Cursed, Immune }
+    enum State { Wander, Patrol, Attack }
 
+    [SerializeField]
+    // Combat Stats
+    public float health;
     public int attack;
     public int physicalDefense;
     public int magicalDefense;
 
-    // movement stats
+    // Speed of movement and time between movements in Patrol/Wander State
     public float speed;
     private float waitTime;
     public float startWaitTime;
 
+    // Patrol State Array
     public Transform[] moveSpots;
+
+    // Wander State iterator
     private int randomSpot;
 
+    // Range of Movement for Wander state
     public float minX;
     public float maxX;
     public float minY;
     public float maxY;
 
+    // Transform of player if following
+    public Transform targetPlayer;
+
+    // Orientation for flipping Sprite
     public bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Set the next position in the array to move to
         randomSpot = Random.Range(0, moveSpots.Length);
 
-        SpriteRenderer spriteColor = GetComponent<SpriteRenderer>();
+        //SpriteRenderer spriteColor = GetComponent<SpriteRenderer>();
 
         //moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
     }
@@ -44,8 +54,56 @@ public class CreatureScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if dead
         CheckHealth();
 
+        switch (currentState)
+     
+    }
+
+
+    // Flip Function for changing sprite orientation
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    // Collision checking with Player Projectile
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8) // Projectile
+        {
+            GameObject projectile = collision.gameObject;
+            health -= projectile.GetComponent<ProjectileScript>().damage;
+            //spriteColor.color = Color.red;
+            Invoke("ChangeColor", 2);
+        }
+    }
+
+    // Check Health to make sure if Dead
+    void CheckHealth()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Change color script for when damaged
+    void ChangeColor()
+    {
+        //spriteColor.color = Color.white;
+    }
+
+    // Enemy States
+    void PatrolState()
+    {
+        // Patrol between different preset locations in the editor at random
+
+        // In Patrol/Wander State, move towards target position in moveSpots array
         transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
 
         if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
@@ -59,7 +117,7 @@ public class CreatureScript : MonoBehaviour
 
                 // Move right, face right
                 if (transform.position.x <= target.x && facingRight != true)
-                {   
+                {
                     Flip();
                 }
                 // Move left, face left
@@ -73,41 +131,18 @@ public class CreatureScript : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
+
     }
 
-    void Flip()
+    void WanderState()
     {
-        facingRight = !facingRight;
-        Vector3 Scaler = transform.localScale;
-        Scaler.x *= -1;
-        transform.localScale = Scaler;
+        // randomly wander around within a set bounds
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void AttackState()
     {
-        if (collision.gameObject.layer == 8) // Projectile
-        {
-            GameObject projectile = collision.gameObject;
-            health -= projectile.GetComponent<ProjectileScript>().damage;
-            spriteColor.color = Color.red;
-            Invoke("ChangeColor", 2);
-
-        }
+        // Follow player if in aggro radius
     }
 
-    void CheckHealth()
-    {
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    void ChangeColor()
-    {
-        spriteColor.color = Color.white;
-
-    }
-
-}
+} // Script End
 
